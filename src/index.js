955d73f7d3c57809722711bd020e84107bbe20be
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-02-06 15:27:43 -0500

    creating a resume project, setting up for my awesome Resume

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+
+// git add . && git commit -m "cooit here" && git push && git status

805f5504abb0d28a3a675fc3f710cdbd4cd501e5
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-02-09 11:53:11 -0500

    My ready view resume

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 
-
 class Footer extends Component {
   render() {
     return (
       <footer>
 
-       <div className="row">
+      <div className="row">
 
-          <div className="twelve columns">
-             <ul className="copyright">
-                <li>&copy; Copyright 2014 CeeVee</li>
-                <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-             </ul>
+         <div className="twelve columns">
 
-          </div>
 
-          <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+            <ul className="copyright">
+               <li>&copy; Copyright 2018 CeeVee</li>
+               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+            </ul>
 
-       </div>
+         </div>
 
-    </footer>
+         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+
+      </div>
+
+   </footer>
     );
   }
 }
